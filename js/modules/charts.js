66e9a914a43efc9bea1678a7cebf0a6a888e3dad
egorplus98-ship-it <egorplus98@gmail.com>
2026-05-31 01:44:55 +0700

    Add files via upload

---
+++ js/modules/charts.js
@@ -0,0 +1,30 @@
+import { state } from '../db.js';
+import { formatDateToDMY, calculate1RM } from '../utils/helpers.js';
+
+var charts = {};
+
+export function updateMaxChart() {
+    console.log('updateMaxChart работает');
+}
+
+export function updateProgressChart() {
+    console.log('updateProgressChart работает');
+}
+
+export function updateTonnageChart() {
+    console.log('updateTonnageChart работает');
+}
+
+export function updateFoodCharts() {
+    console.log('updateFoodCharts работает');
+}
+
+export var weightChart = null;
+
+export function updateWeightChart() {
+    console.log('updateWeightChart работает');
+}
+
+export function setupChartListeners() {
+    console.log('setupChartListeners работает');
+}
